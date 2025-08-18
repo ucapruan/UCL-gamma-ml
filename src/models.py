948086e3581e4aba8a469458f5d779a5015dc183
Shiyi Ruan <sgsruan@pc207.hep.ucl.ac.uk>
2025-08-18 18:07:03 +0100

    Create a .py file to save reused models

---
+++ src/models.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CNN_MDN(nn.Module):
+    """ 1d-CNN + MDN model """
+    def __init__(self, num_elements=3, num_mixtures=3):
+        super().__init__()
+        self.num_elements = num_elements  # D -- dimension of target vector
+        self.num_mixtures = num_mixtures  # M -- number of Gaussian mixture components
+
+        self.conv_layers = nn.Sequential(
+            nn.Conv1d(in_channels=1, out_channels=64, kernel_size=9, padding=4),   # [B, 64, e.g.: 1440 (comes from spectrum)]
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=4),                  # [B, 32, 360]
+
+            nn.Conv1d(64, 128, kernel_size=7, padding=3), # [B, 128, 360]
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2),                  # [B, 128, 180]
+        )
+
+        self.flatten_dim = 128 * 180  # update it if input structure changes
+
+        self.hidden = nn.Sequential(
+            nn.Linear(self.flatten_dim, 256),
+            nn.ReLU(),
+            #nn.Dropout(0.3),
+
+            nn.Linear(256, 64),
+            nn.ReLU(),
+            #nn.Dropout(0.3),
+        )
+
+        # Output heads:
+        self.pi_layer = nn.Linear(64, num_mixtures)                    # [B, M]
+        self.mu_layer = nn.Linear(64, num_mixtures * num_elements)     # [B, M*D]
+        self.sigma_layer = nn.Linear(64, num_mixtures * num_elements)  # [B, M*D]
+
+    def forward(self, x):
+        B = x.size(0)
+        x = self.conv_layers(x)
+        x = x.view(B, -1)
+        x = self.hidden(x)
+
+        # Output parameters : pi, mu, sigma
+        pi = F.softmax(self.pi_layer(x), dim=1)  # [B, M]
+
+        mu = self.mu_layer(x).view(B, self.num_mixtures, self.num_elements)  # [B, M, D]
+        mu = F.softmax(mu, dim=2)  # fraction (0~1, sum=1)
+
+        sigma = torch.exp(self.sigma_layer(x).view(B, self.num_mixtures, self.num_elements))  # [B, M, D]
+
+        return pi, mu, sigma
