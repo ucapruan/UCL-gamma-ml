538b8d0e3ba8a54430967285e0f565b4eba19938
Shiyi Ruan <sgsruan@pc207.hep.ucl.ac.uk>
2025-09-03 22:58:38 +0100

    Add two model variants in models.py

--- src/models.py
+++ src/models.py
@@ -54,3 +54,101 @@ class CNN_MDN(nn.Module):
         sigma = torch.exp(self.sigma_layer(x).view(B, self.num_mixtures, self.num_elements))  # [B, M, D]
 
         return pi, mu, sigma
+    
+
+class deeper_CNN_MDN(nn.Module):
+    def __init__(self, num_elements=3, num_mixtures=3):
+        super().__init__()
+        self.num_elements = num_elements  # D
+        self.num_mixtures = num_mixtures  # M
+
+        self.conv_layers = nn.Sequential(
+            nn.Conv1d(in_channels=1, out_channels=32, kernel_size=5, padding=2),   # [B, 32, 1440]
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2),                  # [B, 32, 720]
+
+            nn.Conv1d(32, 64, kernel_size=3, padding=1),  # [B, 64, 720]
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2),                  # [B, 64, 360]
+
+            nn.Conv1d(64, 128, kernel_size=3, padding=1), # [B, 128, 360]
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2),                  # [B, 128, 180] #avg
+        )
+
+        self.flatten_dim = 128 * 180  # adjust based on input size
+
+        self.hidden = nn.Sequential(
+            nn.Linear(self.flatten_dim, 256),
+            nn.ReLU(),
+            #nn.Dropout(0.3),
+
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            #nn.Dropout(0.3),
+        )
+
+        # Output heads:
+        self.pi_layer = nn.Linear(128, num_mixtures)                    # [B, M]
+        self.mu_layer = nn.Linear(128, num_mixtures * num_elements)     # [B, M*D]
+        self.sigma_layer = nn.Linear(128, num_mixtures * num_elements)  # [B, M*D]
+
+    def forward(self, x):
+        B = x.size(0)
+        x = self.conv_layers(x)
+        x = x.view(B, -1)
+        x = self.hidden(x)
+
+        # Output mixture parameters
+        pi = F.softmax(self.pi_layer(x), dim=1)  # [B, M]
+
+        mu = self.mu_layer(x).view(B, self.num_mixtures, self.num_elements)  # [B, M, D]
+        mu = F.softmax(mu, dim=2)  # Each mixture's output vector sums to 1
+
+        sigma = torch.exp(self.sigma_layer(x).view(B, self.num_mixtures, self.num_elements))  # [B, M, D]
+        return pi, mu, sigma
+    
+
+class Dense_MDN(nn.Module):
+    def __init__(self, input_dim=1440, num_elements=3, num_mixtures=3):
+        super().__init__()
+        self.input_dim = input_dim
+        self.num_elements = num_elements  # D
+        self.num_mixtures = num_mixtures  # M
+
+        self.hidden = nn.Sequential(
+            nn.Linear(self.input_dim, 512),
+            nn.ReLU(),
+
+            nn.Linear(512, 256),
+            nn.ReLU(),
+
+            nn.Linear(256, 128),
+            nn.ReLU(),
+        )
+
+        # Output layers
+        self.pi_layer = nn.Linear(128, num_mixtures)                    # [B, M]
+        self.mu_layer = nn.Linear(128, num_mixtures * num_elements)     # [B, M*D]
+        self.sigma_layer = nn.Linear(128, num_mixtures * num_elements)  # [B, M*D]
+
+    def forward(self, x):
+        B = x.size(0)
+
+        # Flatten
+        if x.dim() == 3:       # [B, 1, 1440]
+            x = x.view(B, -1)  # [B, 1440]
+
+        x = self.hidden(x)
+
+        pi = F.softmax(self.pi_layer(x), dim=1)  # [B, M]
+
+        mu = self.mu_layer(x).view(B, self.num_mixtures, self.num_elements)  # [B, M, D]
+        mu = F.softmax(mu, dim=2)  # 0~1 fraction sum=1
+
+        sigma = torch.exp(self.sigma_layer(x).view(B, self.num_mixtures, self.num_elements))  # [B, M, D]
+
+        return pi, mu, sigma
